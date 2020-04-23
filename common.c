c271c3b7199883294d6476f58af9ec78e4bfc660
sijab <mar.jablonski96@wp.pl>
2020-04-23 11:45:33 +0200

    fix

--- common.c
+++ common.c
@@ -36,14 +36,14 @@ void change_and_measure_thermocouple(void) {
 		}
 	}
 
-	if(flag == 1) {
+	if(flag == 0) {
 		STER_PORT &= ~STER;
 		lcd_locate(0, 0);
 		lcd_str("TERMOPARA TYPU J");
 		lcd_locate(1, 0);
 		lcd_int(measure(average(1)));
 	}
-	else if(flag == 2) {
+	else if(flag == 1) {
 		STER_PORT |= STER;
 		lcd_locate(0, 0);
 		lcd_str("TERMOPARA TYPU K");
